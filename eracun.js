d9019d9c1bf61e55614d96780a065a5b5fc5e1ec
Nejc Šubic <subic9@gmail.com>
2016-05-07 09:54:07 +0000

    Merge izbira-stanke

--- eracun.js
+++ eracun.js
@@ -47,6 +47,10 @@ function davcnaStopnja(izvajalec, zanr) {
 
 // Prikaz seznama pesmi na strani
 streznik.get('/', function(zahteva, odgovor) {
+  
+  if(!zahteva.session.stranka)
+     odgovor.redirect('/prijava');
+
   pb.all("SELECT Track.TrackId AS id, Track.Name AS pesem, \
           Artist.Name AS izvajalec, Track.UnitPrice * " +
           razmerje_usd_eur + " AS cena, \
@@ -112,6 +116,10 @@ var pesmiIzKosarice = function(zahteva, callback) {
 }
 
 streznik.get('/kosarica', function(zahteva, odgovor) {
+  
+  if(!zahteva.session.stranka)
+     odgovor.redirect('/prijava');
+     
   pesmiIzKosarice(zahteva, function(pesmi) {
     if (!pesmi)
       odgovor.sendStatus(500);
@@ -238,12 +246,14 @@ streznik.post('/stranka', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
+    zahteva.session.stranka = polja['seznamStrank'];
     odgovor.redirect('/')
   });
 })
 
 // Odjava stranke
 streznik.post('/odjava', function(zahteva, odgovor) {
+  zahteva.session.stranka = null;
     odgovor.redirect('/prijava') 
 })
 
